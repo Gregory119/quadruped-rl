2f2ce2338ba51e1dc92b7a7d4b49aa6f5daddd54
Gregory Jones <greggie119gj@gmail.com>
2025-09-23 11:52:14 -0400

    create parallel process environments

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import envs
 
 import gymnasium as gym
 
+from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3 import PPO
 
@@ -13,15 +14,18 @@ from os import path
 def main():
     render_mode = 'human'
 
-    env = gym.make("Go1-v0",
-                   render_mode=render_mode,
-                   xml_file=path.join(go1_mj_description.PACKAGE_PATH, "scene.xml"))
-    env.reset()
+    venv = make_vec_env("Go1-v0",
+                        n_envs=2,
+                        env_kwargs={'render_mode': render_mode,
+                                    'xml_file': path.join(go1_mj_description.PACKAGE_PATH,
+                                                          "scene.xml")},
+                        vec_env_cls=SubprocVecEnv,
+                        )
+    venv.reset()
     
-    # venv = SubprocVecEnv([env])
     device = "cpu" # for PPO
     
-    model = PPO("MlpPolicy", env, verbose=0, device=device)
+    model = PPO("MlpPolicy", venv, verbose=0, device=device)
     print("Training...")
     model.learn(total_timesteps=1e6)
     print("Training done")
